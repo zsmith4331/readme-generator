ff75333b5b9f5c55f82cb8d4b3783a6b1288017c
Zach Smith <zsmith4331@gmail.com>
2021-02-07 16:15:46 -0500

    fixed table of contents

--- index.js
+++ index.js
@@ -13,20 +13,9 @@ inquirer.prompt([
         name: "description"
     },
     {
-        type: "checkbox",
-        message: "Select the sections that shall be included in the table of contents.",
-        name: "table",
-        choices: [
-            "Installation",
-            "Usage",
-            "License",
-            "Contributions",
-            "Tests"]
-    },
-    {
         type: "input",
         message: "What are the installation instrucitons?",
-        name: "instructions"
+        name: "installation"
     },
     {
 
@@ -65,34 +54,40 @@ inquirer.prompt([
         name: "email"
     }
     
-]).then(respone => {
+]).then(response => {
     const content = `
-# ${respone.title}
-
-## Description
-${respone.description}
+# ${response.title}
 
 ## Table of Contents
-${respone.table}
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions & Contact Information](#questions)
 
-## Instructions
-${respone.instructions}
+## Description
+${response.description}
 
-## Useage Information
-${respone.usage}
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
 
 ## License
-${respone.license}
+${response.license}
 
 ## Contributors
-${respone.contributors}
+${response.contributors}
 
 ## Testing
-${respone.test}
+${response.test}
 
 ## Questions
 Please see Github Profile and Email Address below to ask questions  
-Github Profile: ${respone.github} | Email: ${respone.email}
+Github Profile: ${response.github} | Email: ${response.email}
     `;   
     
     fs.writeFile('README.md', content, err => {
@@ -100,3 +95,19 @@ Github Profile: ${respone.github} | Email: ${respone.email}
         else console.log('success!');
       });
 });
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
